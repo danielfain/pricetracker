5ed7680b0ae5e49abc9dc51b68279787893a9900
Daniel Fain <daniel@fain.dev>
2020-12-31 10:27:05 -0500

    Create model for an item that is being tracked

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+    requested_price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    current_price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
