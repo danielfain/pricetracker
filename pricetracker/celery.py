8294a7645abdae1e8a0dcc7ab8a2af43c1ca78cb
Daniel Fain <daniel@fain.dev>
2020-12-31 12:18:25 -0500

    Configure update price task to run every minute in the background

--- pricetracker/celery.py
+++ pricetracker/celery.py
@@ -1,9 +1,17 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pricetracker.settings")
 
 app = Celery("pricetracker")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "update-item-prices": {
+        "task": "tracker.tasks.update_prices",
+        "schedule": 60.0,
+    }
+}
