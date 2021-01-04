a609b974104215ab20a18a5259c0c2636ec80370
Daniel Fain <daniel@fain.dev>
2021-01-04 06:50:41 -0500

    Determine if an item is active or not

--- tracker/models.py
+++ tracker/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     url = models.URLField(max_length=200)
     requested_price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
     current_price = models.DecimalField(max_digits=8, decimal_places=2)
+    active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
