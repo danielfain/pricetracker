491de03637150dcfab61931be3365809652c8e46
Daniel Fain <daniel@fain.dev>
2021-01-04 06:22:58 -0500

    Fix bug where ended listings arent checked for

--- tracker/tasks.py
+++ tracker/tasks.py
@@ -28,8 +28,9 @@ def crawl(url):
     html = urlopen(req).read()
     bs = BeautifulSoup(html, "html.parser")
 
-    title = bs.find("h1", id="itemTitle").get_text().replace("Details about", "")
-    price = bs.find("span", id="prcIsum").get_text()
-    clean_price = float(price.strip().replace("US", "").replace("$", ""))
+    if not bs.find("div", {"class": "app-cvip-message-container"}) is None:
+        title = bs.find("h1", id="itemTitle").get_text().replace("Details about", "")
+        price = bs.find("span", id="prcIsum").get_text()
+        clean_price = float(price.strip().replace("US", "").replace("$", ""))
 
-    return {"title": title, "current_price": clean_price}
+        return {"title": title, "current_price": clean_price}
